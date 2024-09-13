87e900098201594cc49efa76cf4ba5684422ffaf
Shylock Hg <tcath2s@icloud.com>
2024-09-13 08:58:41 +0800

    Remove unused index.

--- src/ident_pool.rs
+++ src/ident_pool.rs
@@ -3,28 +3,26 @@ use indexmap::IndexSet;
 struct Symbol(u32);
 
 struct IdentPool {
-    id: u32,
-    map: IndexSet<String>,
+    inner: IndexSet<String>,
 }
 
 impl IdentPool {
     fn new() -> Self {
         IdentPool {
-            id: 0,
-            map: IndexSet::default(),
+            inner: IndexSet::default(),
         }
     }
 
     fn create(&mut self, ident: &str) -> Symbol {
-        if self.map.insert(ident.to_owned()) {
-            Symbol((self.map.len() - 1) as u32)
+        if self.inner.insert(ident.to_owned()) {
+            Symbol((self.inner.len() - 1) as u32)
         } else {
-            Symbol(self.map.get_index_of(ident).unwrap() as u32)
+            Symbol(self.inner.get_index_of(ident).unwrap() as u32)
         }
     }
 
     fn get_str(&self, s: &Symbol) -> Option<&String> {
-        self.map.get_index(s.0 as usize)
+        self.inner.get_index(s.0 as usize)
     }
 }
 
@@ -41,7 +39,7 @@ mod tests {
         assert_eq!(pool.get_str(&symbol1).unwrap(), "symbol1");
 
         // repeat
-        let symbol1 = pool.create("symbol1");
+        pool.create("symbol1");
         assert_eq!(symbol1.0, 0);
         assert_eq!(pool.get_str(&symbol1).unwrap(), "symbol1");
 
@@ -50,7 +48,6 @@ mod tests {
         assert_eq!(symbol2.0, 1);
         assert_eq!(pool.get_str(&symbol2).unwrap(), "symbol2");
 
-        // another
         assert_eq!(pool.get_str(&symbol1).unwrap(), "symbol1");
     }
 }
