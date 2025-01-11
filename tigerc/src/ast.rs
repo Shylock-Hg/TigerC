8031eb9a0951be49865e8452b01e830a02d48796
Shylock Hg <tcath2s@icloud.com>
2025-01-11 12:04:09 +0800

    feat: impl Display for AST.

--- tigerc/src/ast.rs
+++ tigerc/src/ast.rs
@@ -1,8 +1,19 @@
+use std::fmt::Display;
+
 use crate::ident_pool::Symbol;
 
 pub enum Ast {
     Decl(Decl),
     Expr(Expr),
+}
+
+impl Display for Ast {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Ast::Decl(decl) => write!(f, "{}", decl),
+            Ast::Expr(expr) => write!(f, "{}", expr),
+        }
+    }
 }
 
 pub enum Value {
@@ -14,8 +25,27 @@ pub enum Value {
     Str(String),
 }
 
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::Nothing => write!(f, "()"),
+            Value::Nil => write!(f, "nil"),
+            Value::Int(i) => write!(f, "{}", i),
+            Value::Str(s) => write!(f, "{}", s),
+        }
+    }
+}
+
 pub enum Unary {
     Negative(Box<Expr>),
+}
+
+impl Display for Unary {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Unary::Negative(expr) => write!(f, "-{}", expr),
+        }
+    }
 }
 
 pub enum Binary {
@@ -35,10 +65,44 @@ pub enum Binary {
     Or(Box<Expr>, Box<Expr>),
 }
 
+impl Display for Binary {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Binary::Add(lhs, rhs) => write!(f, "{} + {}", lhs, rhs),
+            Binary::Minus(lhs, rhs) => write!(f, "{} - {}", lhs, rhs),
+            Binary::Multiply(lhs, rhs) => write!(f, "{} * {}", lhs, rhs),
+            Binary::Divide(lhs, rhs) => write!(f, "{} / {}", lhs, rhs),
+
+            Binary::Eq(lhs, rhs) => write!(f, "{} = {}", lhs, rhs),
+            Binary::Ne(lhs, rhs) => write!(f, "{} <> {}", lhs, rhs),
+            Binary::Gt(lhs, rhs) => write!(f, "{} > {}", lhs, rhs),
+            Binary::Ge(lhs, rhs) => write!(f, "{} >= {}", lhs, rhs),
+            Binary::Lt(lhs, rhs) => write!(f, "{} < {}", lhs, rhs),
+            Binary::Le(lhs, rhs) => write!(f, "{} <= {}", lhs, rhs),
+
+            Binary::And(lhs, rhs) => write!(f, "{} & {}", lhs, rhs),
+            Binary::Or(lhs, rhs) => write!(f, "{} | {}", lhs, rhs),
+        }
+    }
+}
+
 // RecordExpr => <ident> "{" [<ident> "=" expr, ...] "}"
 pub struct RecordExpr {
     ty: Symbol,
     init: Vec<(Symbol, Expr)>,
+}
+
+impl Display for RecordExpr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {{", self.ty)?;
+        for (i, (field, expr)) in self.init.iter().enumerate() {
+            write!(f, "{} = {}", field, expr)?;
+            if i < (self.init.len() - 1) {
+                write!(f, ", ")?;
+            }
+        }
+        write!(f, "}}")
+    }
 }
 
 // ArrayExpr => <ident> [expr] of expr
@@ -49,6 +113,13 @@ pub struct ArrayExpr {
     init: Box<Expr>,
 }
 
+impl Display for ArrayExpr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} [{}]", self.ty, self.len)?;
+        write!(f, " of {}", self.init)
+    }
+}
+
 // "if" expr "then" expr "else" expr
 pub struct IfThenElseExpr {
     condition: Box<Expr>,
@@ -56,15 +127,38 @@ pub struct IfThenElseExpr {
     el: Box<Expr>,
 }
 
+impl Display for IfThenElseExpr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "if {} then {} else {}",
+            self.condition, self.then, self.el
+        )
+    }
+}
+
 // "if" expr "then" expr
 pub struct IfThen {
     condition: Box<Expr>,
     then: Box<Expr>,
 }
+
+impl Display for IfThen {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "if {} then {}", self.condition, self.then)
+    }
+}
+
 // "while" expr "do" expr
 pub struct While {
     condition: Box<Expr>,
     body: Box<Expr>,
+}
+
+impl Display for While {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "while {} do {}", self.condition, self.body)
+    }
 }
 
 // "for" <ident> ":=" expr "to" expr "do" expr
@@ -75,10 +169,37 @@ pub struct For {
     body: Box<Expr>,
 }
 
+impl Display for For {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "for {} := {} to {} do {}",
+            self.local, self.lower, self.upper, self.body
+        )
+    }
+}
+
 // "let" decls "in" expr[;expr...] "end"
 pub struct Let {
     decls: Vec<Decl>,
     sequence: Vec<Expr>,
+}
+
+impl Display for Let {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "let ")?;
+        for (i, decl) in self.decls.iter().enumerate() {
+            write!(f, "{} ", decl)?;
+        }
+        write!(f, " in ")?;
+        for (i, expr) in self.sequence.iter().enumerate() {
+            write!(f, "{}", expr)?;
+            if i < (self.sequence.len() - 1) {
+                write!(f, "; ")?;
+            }
+        }
+        write!(f, " end")
+    }
 }
 
 // lvalue => <ident>
@@ -90,10 +211,20 @@ pub enum LeftValue {
     Subscript(Box<LeftValue>, Box<Expr>),
 }
 
+impl Display for LeftValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LeftValue::Variable(ident) => write!(f, "{}", ident),
+            LeftValue::Field(lvalue, field) => write!(f, "{}.{}", lvalue, field),
+            LeftValue::Subscript(lvalue, expr) => write!(f, "{}[{}]", lvalue, expr),
+        }
+    }
+}
+
 pub enum Expr {
     Literal(Value),
     LeftValue(LeftValue),
-    // (exp1, exp2, ...)
+    // (exp1; exp2; ...)
     Sequence(Vec<Expr>),
     Unary(Unary),
     Binary(Binary),
@@ -110,6 +241,47 @@ pub enum Expr {
     Parenthesis(Box<Expr>),
 }
 
+impl Display for Expr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expr::Literal(value) => write!(f, "{}", value),
+            Expr::LeftValue(lvalue) => write!(f, "{}", lvalue),
+            Expr::Sequence(exprs) => {
+                write!(f, "(")?;
+                for (i, expr) in exprs.iter().enumerate() {
+                    write!(f, "{}", expr)?;
+                    if i < (exprs.len() - 1) {
+                        write!(f, "; ")?;
+                    }
+                }
+                write!(f, ")")
+            }
+            Expr::Unary(unary) => write!(f, "{}", unary),
+            Expr::Binary(binary) => write!(f, "{}", binary),
+            Expr::FuncCall(func, args) => {
+                write!(f, "{}(", func)?;
+                for (i, arg) in args.iter().enumerate() {
+                    write!(f, "{}", arg)?;
+                    if i < (args.len() - 1) {
+                        write!(f, ", ")?;
+                    }
+                }
+                write!(f, ")")
+            }
+            Expr::RecordExpr(record) => write!(f, "{}", record),
+            Expr::ArrayExpr(array) => write!(f, "{}", array),
+            Expr::Assign(lvalue, expr) => write!(f, "{} := {}", lvalue, expr),
+            Expr::IfThenElse(ite) => write!(f, "{}", ite),
+            Expr::IfThen(cond, then) => write!(f, "if {} then {}", cond, then),
+            Expr::While(while_expr) => write!(f, "{}", while_expr),
+            Expr::For(for_expr) => write!(f, "{}", for_expr),
+            Expr::Break => write!(f, "break"),
+            Expr::Let(let_expr) => write!(f, "{}", let_expr),
+            Expr::Parenthesis(expr) => write!(f, "({})", expr),
+        }
+    }
+}
+
 // Decl => TypeDecl
 //      => VarDecl
 //      => FuncDecl
@@ -117,6 +289,16 @@ pub enum Decl {
     Type(TypeDecl),
     Var(VarDecl),
     Func(FuncDecl),
+}
+
+impl Display for Decl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Decl::Type(ty) => write!(f, "{}", ty),
+            Decl::Var(var) => write!(f, "{}", var),
+            Decl::Func(func) => write!(f, "{}", func),
+        }
+    }
 }
 
 // VarDecl => "var" <ident> ":=" expr
@@ -127,6 +309,15 @@ pub struct VarDecl {
     pub init: Expr,
 }
 
+impl Display for VarDecl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.ty {
+            Some(ty) => write!(f, "var {} : {} := {}", self.name, ty, self.init),
+            None => write!(f, "var {} := {}", self.name, self.init),
+        }
+    }
+}
+
 // FuncDecl => "function" <ident> "(" TypeFields ")" "=" expr
 //          => "function" <ident> "(" TypeFields ")" ":" <ident> "=" expr
 pub struct FuncDecl {
@@ -134,6 +325,23 @@ pub struct FuncDecl {
     pub args: Vec<Field>,
     pub ret_ty: Option<Symbol>,
     pub body: Expr,
+}
+
+impl Display for FuncDecl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "function {} (", self.name)?;
+        for (i, arg) in self.args.iter().enumerate() {
+            write!(f, "{}: {}", arg.name, arg.ty)?;
+            if i < (self.args.len() - 1) {
+                write!(f, ", ")?;
+            }
+        }
+        write!(f, ")")?;
+        match &self.ret_ty {
+            Some(ty) => write!(f, ": {} = {}", ty, self.body),
+            None => write!(f, " = {}", self.body),
+        }
+    }
 }
 
 // TypeDecl => "type" IDENT = Ty
@@ -147,15 +355,50 @@ pub struct TypeDecl {
     pub ty: Ty,
 }
 
+impl Display for TypeDecl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "type {} = {}", self.type_name, self.ty)
+    }
+}
+
 pub enum Ty {
     Name(Symbol),
     Struct(TyStruct),
     Array(Symbol),
 }
 
+impl Display for Ty {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Ty::Name(name) => write!(f, "{}", name),
+            Ty::Struct(fields) => write!(f, "{}", fields),
+            Ty::Array(ty) => write!(f, "array of {}", ty),
+        }
+    }
+}
+
 pub struct TyStruct(pub Vec<Field>);
+
+impl Display for TyStruct {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{")?;
+        for (i, field) in self.0.iter().enumerate() {
+            write!(f, "{}", field)?;
+            if i < (self.0.len() - 1) {
+                write!(f, ", ")?;
+            }
+        }
+        write!(f, "}}")
+    }
+}
 
 pub struct Field {
     pub name: Symbol,
     pub ty: Symbol,
+}
+
+impl Display for Field {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", self.name, self.ty)
+    }
 }
