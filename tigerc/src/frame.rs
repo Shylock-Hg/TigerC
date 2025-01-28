9e6140e0e724e7488c4ad86b371e64c71c57c2c2
Shylock Hg <tcath2s@icloud.com>
2025-01-28 10:48:35 +0800

    feat: add trait Frame.

--- tigerc/src/frame.rs
+++ tigerc/src/frame.rs
@@ -1,26 +1,30 @@
-use std::ops::Index;
-
 use indexmap::IndexMap;
 
 use crate::ident_pool::{self, Symbol};
 
-enum Access {
+pub enum Access {
     // offset to frame pointer
     Frame(usize),
     // register notion
     Register(Register),
 }
 
-struct Register(Symbol);
+pub struct Register(pub Symbol);
 
-struct Variable {
+pub struct Variable {
     // true for variable that must be addressed
-    escape: bool,
-    access: Access,
+    pub escape: bool,
+    pub access: Access,
+}
+
+pub trait Frame {
+    fn new(name: Symbol, parameters: IndexMap<Symbol, Variable>) -> Self;
+    fn name(&self) -> Symbol;
+    fn allocate_local(&mut self, symbol: Symbol, var: Variable);
 }
 
 // frame of a function
-struct Frame {
+struct FrameAmd64 {
     // function name
     name: Symbol,
     // parameters
@@ -31,9 +35,9 @@ struct Frame {
     id: usize,
 }
 
-impl Frame {
-    pub fn new(name: Symbol, parameters: IndexMap<Symbol, Variable>) -> Self {
-        Frame {
+impl Frame for FrameAmd64 {
+    fn new(name: Symbol, parameters: IndexMap<Symbol, Variable>) -> Self {
+        FrameAmd64 {
             name,
             parameters,
             locals: IndexMap::new(),
@@ -41,7 +45,11 @@ impl Frame {
         }
     }
 
-    pub fn allocate_local(&mut self, symbol: Symbol, var: Variable) {
+    fn name(&self) -> Symbol {
+        self.name
+    }
+
+    fn allocate_local(&mut self, symbol: Symbol, var: Variable) {
         if self.locals.contains_key(&symbol) {
             self.locals.insert(
                 ident_pool::create_symbol(&format!("{}:{}", symbol, self.id)),
